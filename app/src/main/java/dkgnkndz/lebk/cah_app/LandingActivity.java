76563f1f1d46751aa0cae99f59d4bc5110fcb8a2
Sören Jochimsen <sjochimsen@vectron.de>
2018-10-08 14:52:22 +0200

    Initial commit

---
+++ app/src/main/java/dkgnkndz/lebk/cah_app/LandingActivity.java
@@ -0,0 +1,13 @@
+package dkgnkndz.lebk.cah_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
