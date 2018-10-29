1427031b234c74de56c2589224fa112fee0fa38b
Sören Jochimsen <sjochimsen@vectron.de>
2018-10-29 16:37:12 +0100

    refactored gui components

--- app/src/main/java/dkgnkndz/lebk/cah_app/activity/LandingActivity.java
+++ app/src/main/java/dkgnkndz/lebk/cah_app/activity/LandingActivity.java
@@ -2,24 +2,31 @@ package dkgnkndz.lebk.cah_app.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import dkgnkndz.lebk.cah_app.MyApp;
 import dkgnkndz.lebk.cah_app.R;
-import dkgnkndz.lebk.cah_app.network.thread.NetworkWorker;
 import protocol.object.request.StartGameRequest;
 import protocol.object.response.StartGameResponse;
 
 public class LandingActivity extends ActivityBase {
     
-    @BindView(R.id.nickname)
-    EditText nickname;
+    @BindView(R.id.nicknameEdit)
+    EditText nicknameEdit;
+
+    @BindView(R.id.playButton)
+    Button playButton;
 
     @BindView(R.id.waitForGameProgressBar)
     ProgressBar progressBar;
+
+    @BindView(R.id.waitForGameMessageView)
+    TextView waitForGameMessageView;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -30,9 +37,9 @@ public class LandingActivity extends ActivityBase {
     }
 
     public void play(final View view) {
-        MyApp myApp = (MyApp)getApplication();
+        final MyApp myApp = (MyApp)getApplication();
 
-        final String nickName = this.nickname.getText().toString();
+        final String nickName = nicknameEdit.getText().toString();
         final StartGameRequest startGameRequest = new StartGameRequest();
         startGameRequest.nickName = nickName;
 
@@ -48,6 +55,13 @@ public class LandingActivity extends ActivityBase {
     }
 
     public void onWaitForGame() {
-        progressBar.setVisibility(View.VISIBLE);
+        toggleVisibility();
+    }
+
+    private void toggleVisibility() {
+        nicknameEdit.setVisibility((nicknameEdit.getVisibility() == View.VISIBLE) ? View.INVISIBLE : View.VISIBLE);
+        playButton.setVisibility((playButton.getVisibility() == View.VISIBLE) ? View.INVISIBLE : View.VISIBLE);
+        progressBar.setVisibility((progressBar.getVisibility() == View.VISIBLE) ? View.INVISIBLE : View.VISIBLE);
+        waitForGameMessageView.setVisibility((waitForGameMessageView.getVisibility() == View.VISIBLE) ? View.INVISIBLE : View.VISIBLE);
     }
 }
