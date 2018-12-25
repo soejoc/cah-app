e89d6589278d357ad7fe324d515c0e1453ddab1a
Collo <soeren@jochimsen.email>
2018-12-25 19:57:26 +0100

    Encrypted the connection via ssl

--- app/src/main/java/dkgnkndz/lebk/cah_app/network/thread/NetworkWorker.java
+++ app/src/main/java/dkgnkndz/lebk/cah_app/network/thread/NetworkWorker.java
@@ -1,8 +1,11 @@
 package dkgnkndz.lebk.cah_app.network.thread;
 
+import android.util.Log;
+
 import codec.decoder.meta.MetaDecoder;
 import codec.encoder.meta.MetaEncoder;
 import dkgnkndz.lebk.cah_app.network.handler.MessageHandler;
+import initializer.SslClientInitializer;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -11,12 +14,17 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import protocol.object.message.ProtocolMessage;
 
 public class NetworkWorker implements Runnable {
     private final String host;
     private final int port;
     private final ProtocolMessage initialMessage;
+
+    private static final String TAG = "NetworkWorker";
 
     public NetworkWorker(final String host, final int port, final ProtocolMessage initialMessage) {
         this.host = host;
@@ -29,27 +37,20 @@ public class NetworkWorker implements Runnable {
         final EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
+            final SslContext sslCtx = SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+
             final Bootstrap b = new Bootstrap();
             b.group(workerGroup);
             b.channel(NioSocketChannel.class);
             b.option(ChannelOption.SO_KEEPALIVE, true);
-            b.handler(new ChannelInitializer<SocketChannel>() {
-
-                @Override
-                public void initChannel(final SocketChannel ch) {
-                    ch.pipeline().addLast(
-                            new MetaDecoder(),
-                            new MetaEncoder(),
-                            new MessageHandler(initialMessage)
-                    );
-                }
-            });
+            b.handler(new SslClientInitializer(sslCtx, new MessageHandler(initialMessage), host, port));
 
             final ChannelFuture f = b.connect(host, port).sync();
 
             f.channel().closeFuture().sync();
-        } catch (final InterruptedException e) {
-
+        } catch (final Throwable throwable) {
+            Log.d(TAG, throwable.getMessage());
         }
         finally {
             workerGroup.shutdownGracefully();
