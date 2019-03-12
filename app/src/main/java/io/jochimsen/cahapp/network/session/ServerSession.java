930207857b27da2042c56ff23fb8bb39f138b122
Sören Jochimsen <soeren@jochimsen.email>
2019-03-13 00:16:29 +0100

    new explicit type for sending messages (RequestMessage)

--- app/src/main/java/io/jochimsen/cahapp/network/session/ServerSession.java
+++ app/src/main/java/io/jochimsen/cahapp/network/session/ServerSession.java
@@ -1,9 +1,10 @@
 package io.jochimsen.cahapp.network.session;
 
+import io.jochimsen.cahframework.protocol.object.message.RequestMessage;
 import io.jochimsen.cahframework.session.Session;
 import io.netty.channel.ChannelHandlerContext;
 
-public class ServerSession extends Session {
+public class ServerSession extends Session<RequestMessage> {
     public ServerSession(final ChannelHandlerContext channelHandlerContext) {
         super(channelHandlerContext);
     }
