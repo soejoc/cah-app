fc46b6f853637c5be4086ad6f7a03c7752f0e05f
Sören Jochimsen <soeren@jochimsen.email>
2019-03-18 12:09:57 +0100

    more concrete incoming massage type

--- app/src/main/java/io/jochimsen/cahapp/repository/ProtocolMessageRepository.java
+++ app/src/main/java/io/jochimsen/cahapp/repository/ProtocolMessageRepository.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 import io.jochimsen.cahapp.di.scope.AppScope;
 import io.jochimsen.cahframework.protocol.object.message.ProtocolMessage;
+import io.jochimsen.cahframework.protocol.object.message.ResponseMessage;
 import io.reactivex.Flowable;
 import io.reactivex.processors.FlowableProcessor;
 import io.reactivex.processors.PublishProcessor;
@@ -11,7 +12,7 @@ import io.reactivex.processors.PublishProcessor;
 @AppScope
 public final class ProtocolMessageRepository {
 
-    private final FlowableProcessor<ProtocolMessage> protocolMessageSubject = PublishProcessor.create();
+    private final FlowableProcessor<ResponseMessage> messageProcessor = PublishProcessor.create();
 
     @Inject
     public ProtocolMessageRepository() {
@@ -19,12 +20,12 @@ public final class ProtocolMessageRepository {
 
     @SuppressWarnings("unchecked")
     public <T extends ProtocolMessage> Flowable<T> observe(final Class<T> clazz) {
-        return protocolMessageSubject
+        return messageProcessor
                 .filter(protocolMessage -> protocolMessage.getClass().equals(clazz))
                 .map(protocolMessage -> (T)protocolMessage);
     }
 
-    public void emit(final ProtocolMessage protocolMessage) {
-        protocolMessageSubject.onNext(protocolMessage);
+    public void emit(final ResponseMessage message) {
+        messageProcessor.onNext(message);
     }
 }
